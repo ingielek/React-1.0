47dfc0b2b75d0b61c5e0d666702fe53ca0ae486c
ingielek <ingielewicz.p@gmail.com>
2017-10-10 22:26:38 +0200

    next commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { createStore } from 'redux'
+import {Provider } from 'react-redux';
+import {eventsReducer} from "./reducers/eventsReducer";
 import App from './App';
 
+const store = createStore(eventsReducer);
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+
+ReactDOM.render(
+    <Provider store={store}>
+    <App/>
+    </Provider>,
+    document.getElementById('root'));
---
+++ src/reducers/eventsReducer.js
@@ -0,0 +1,13 @@
+import * as constants from '../constants'
+import events from '../data/events.json'
+const initialState = {
+    events
+};
+export function eventsReducer ( state = initialState, action) {
+        switch (action.type) {
+            case constants.EVENTS_CLEAR:
+                return { ...state, events: [] };
+            default:
+                return state;
+        }
+}
